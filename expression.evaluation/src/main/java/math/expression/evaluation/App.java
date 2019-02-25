3307c087a4b53ac5c414b39b1846955ef52432f7
brijesh <brijeshsrivastava21@gmail.com>
2019-02-25 17:13:30 +0530

    App File updated

--- expression.evaluation/src/main/java/math/expression/evaluation/App.java
+++ expression.evaluation/src/main/java/math/expression/evaluation/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+         System.out.println(BinaryExpression.evaluate("10 + 2 * 6")); 
+        System.out.println(BinaryExpression.evaluate("100 * 2 + 12")); 
+        System.out.println(BinaryExpression.evaluate("100 * ( 2 + 12 )")); 
+        System.out.println(BinaryExpression.evaluate("100 * ( 2 + 12 ) / 14")); 
     }
 }
